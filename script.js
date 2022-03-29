956f8b24ce0cb9d7294780852fbbb218da273fd8
YauheniWind <78211102+YauheniWind@users.noreply.github.com>
2022-03-29 13:24:52 +0200

    fix code

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const fs = require('fs');
+const path = require('path');
 
 const { createGzip } = require('zlib');
 const { pipeline } = require('stream');
 
 const gzip = createGzip();
-const source = fs.createReadStream(process.argv[2])
-const destination = fs.createWriteStream(process.argv[3] + '.zip');
+const source = fs.createReadStream(path.resolve(process.argv[2]));
+const destination = fs.createWriteStream(path.resolve(process.argv[3]) + '.zip');
 
 pipeline(source, gzip, destination, (err) => {
     if (err) {
